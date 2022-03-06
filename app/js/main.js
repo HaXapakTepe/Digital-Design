b1e75463569d6998dedefa0ffe99f58b6a529d16
HaXapakTepe <haxapaktepe@gmail.com>
2022-03-06 21:35:13 +0400

    added arrow

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
-$ (function() {
+(function () {
+  'use strict';
 
-});
+  function trackScroll() {
+    const scrolled = window.pageYOffset;
+    const coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      goTopBtn.classList.add('arrow-up__show');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('arrow-up__show');
+    }
+  }
+
+  const goTopBtn = document.querySelector('.arrow-up');
+
+  window.addEventListener('scroll', trackScroll);
+})();
